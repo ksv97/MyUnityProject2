023716c7c2df25614a69117429460c2bf00def9e
ksv97 <kosov.r.m@gmail.com>
2020-10-23 18:52:10 +0300

    Test

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotateAmount;
+
+    float rotation;
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 vector3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (vector3.x < 0)
+            {
+                rotation = rotateAmount;
+            }
+            else rotation = -rotateAmount;
+
+            transform.Rotate(0, 0, rotation);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.up * moveSpeed;   
+    }
+}
